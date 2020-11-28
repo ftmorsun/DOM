98ab643c5e20029080cdbd96ae5e8cb5bdb8727d
Fatma Orsun <fatmaorsun2@gmail.com>
2020-11-28 14:41:53 -0500

    "DOM with JS"

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var radius = prompt("enter radius: ");
 //find the area of square
 var side = prompt("Enter side of square");
 console.log("Area of Square " + side * side);
-var areaofrec= alert(“The are of rec is:“+ (s1*s2));
+var areaofrec= alert("The are of rec is:"+ (s1*s2));
 
 //find the perimeter of rectangle
 var side1 = prompt("side1: ");
